49b01888e406954622e705743721312177cb1a82
Offliner <42777325+Offliners@users.noreply.github.com>
2020-09-01 12:46:53 +0800

    Update Lab2.m

--- Week 5/Experiment-4-1/Lab2.m
+++ Week 5/Experiment-4-1/Lab2.m
@@ -1,6 +1,7 @@
 G1 = tf([25], [1 4 25])
 pole1 = roots([1 4 25]);
 real2 = [real(pole1(1))+pole1(1) real(pole1(2))+pole1(2)];
-img3 = [real(pole1(1)*(-0.5))+pole1(1) real(pole1(2)*(-0.5))+pole1(2)];
+real3 = [real(pole1(1)*(-0.5))+pole1(1) real(pole1(2)*(-0.5))+pole1(2)];
 G2 = tf(real2(1)*real2(2), poly([real2(1) real2(2)]))
-G3 = tf(img3(1)*img3(2), poly([img3(1) img3(2)]))
+G3 = tf(real3(1)*real3(2), poly([real3(1) real3(2)]))
+
